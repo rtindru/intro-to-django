6046dd07f7d284bbb38fac2925fbe5465eca07c7
Rajtilak Indrajit <rt.indru@gmail.com>
2016-11-03 21:01:19 +0530

    Added new expensify app; added custom model

--- django_101/django_101/urls.py
+++ django_101/django_101/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hello/', include('hello_django.urls'))
+    url(r'^hello/', include('hello_django.urls')),
+    url(r'^expensify/', include('expensify.urls')),
 ]
---
+++ django_101/expensify/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+TRANSACTION_TYPES = (
+    ('CR', 'Credit'),
+    ('DE', 'Debit'),
+)
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    type = models.CharField(choices=TRANSACTION_TYPES, default='DE', max_length=3)
+    description = models.TextField(null=True, blank=True, default=None)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
