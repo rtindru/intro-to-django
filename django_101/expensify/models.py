12fbaa859cd9411334a06b59b015d5bd04b897e1
Rajtilak Indrajit <rt.indru@gmail.com>
2016-11-04 15:36:03 +0530

    Fixed created_at and updated_at auto add fields

--- django_101/expensify/models.py
+++ django_101/expensify/models.py
@@ -13,8 +13,8 @@ class Transaction(models.Model):
     amount = models.DecimalField(decimal_places=2, max_digits=10)
     type = models.CharField(choices=TRANSACTION_TYPES, default='DE', max_length=3)
     description = models.TextField(null=True, blank=True, default=None)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return "{}, {}-{}".format(self.user, self.amount, self.type)
