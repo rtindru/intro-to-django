5f3f4f18f869162e3c011a6ecb95f3be871ed6f7
Rajtilak Indrajit <rt.indru@gmail.com>
2016-11-04 15:33:24 +0530

    Added trailing comma for better diffs

--- django_101/hello_django/urls.py
+++ django_101/hello_django/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     url(r'^$', views.hello_django, name='hello_django'),
     url(r'^user/(?P<user>[\w]+)', views.hello_user, name='hello_user'),
     url(r'^template/(?P<user>[\w]+)', views.hello_template, name='hello_template'),
-    url(r'^template-two/(?P<user>[\w]+)', views.hello_template_two, name='hello_template')
-
+    url(r'^template-two/(?P<user>[\w]+)', views.hello_template_two, name='hello_template'),
 ]
